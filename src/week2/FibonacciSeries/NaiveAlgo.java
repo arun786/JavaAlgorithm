fc4c4164370483fa34cd4a538cbe10a6e6709209
arun789 <adwiti1975@gmail.com>
2017-11-04 17:15:01 -0700

    Inefficient way of calculating Algorithm

---
+++ src/week2/FibonacciSeries/NaiveAlgo.java
@@ -0,0 +1,19 @@
+package week2.FibonacciSeries;
+
+import java.math.BigInteger;
+
+public class NaiveAlgo {
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(50));
+    }
+
+
+    public static Integer fibonacci(Integer num) {
+        if (num <= 1) {
+            return num;
+        }
+
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+}
