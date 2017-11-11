f6c6885ccfbd7a6789731cb2f85411c5c430165e
arun789 <adwiti1975@gmail.com>
2017-11-11 00:27:06 -0700

    Efficient way o calculating Fibonacci series

--- src/week2/Fibonacci/EfficientAlgo.java
+++ src/week2/Fibonacci/EfficientAlgo.java
@@ -1,22 +1,30 @@
 package week2.Fibonacci;
 
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class EfficientAlgo {
 
     public static void main(String[] args) {
-        System.out.println(fibonacci(50)[49]);
-    }
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int size = num + 1;
 
-    public static BigInteger[] fibonacci(Integer num) {
+        BigInteger aNum[] = new BigInteger[size];
 
-        BigInteger aNum[] = new BigInteger[num];
-        for (int i = 0; i < num; i++) {
-            if (i <= 2) {
+        if (num == 0) {
+            aNum[num] = BigInteger.valueOf(num);
+        }
+
+
+        for (int i = 0; i < size; i++) {
+            if (i < 2) {
                 aNum[i] = BigInteger.valueOf(i);
             } else
                 aNum[i] = aNum[i - 1].add(aNum[i - 2]);
         }
-        return aNum;
+
+        BigInteger result = aNum[num];
+        System.out.println(result);
     }
 }
